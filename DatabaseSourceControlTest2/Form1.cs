89fd943dd1d3e7fb1e59e786e1a9918c02cd685b
jhoon kwak <liljk@users.noreply.github.com>
2018-01-16 22:05:12 -0800

    Added a greeting message

--- DatabaseSourceControlTest2/Form1.cs
+++ DatabaseSourceControlTest2/Form1.cs
@@ -19,7 +19,7 @@ namespace DatabaseSourceControlTest2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Hello World!!!");
         }
     }
 }
